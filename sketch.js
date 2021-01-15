f493a1cd2f1f718f5fef78da4bd72e662b581f6d
Baani140208 <baani6763@gmail.com>
2021-01-15 16:53:36 +0530

    modified

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6
+var rope
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +39,12 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    log6 = new Log(230,180,80,PI/2)
+
+    rope = new Chain(bird.body,log6.body)
+    
+   
+
 }
 
 function draw(){
@@ -61,4 +70,7 @@ function draw(){
 
     bird.display();
     platform.display();
+    log6.display();
+    rope.display();
+   
 }
